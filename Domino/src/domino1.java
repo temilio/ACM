6f9f068a27ea0978631ada139fead4c514d3b010
emilio torres <piscraf26@gmail.com>
2015-10-06 17:43:26 -0300

    primer intento para subir ( ACM domino )

---
+++ Domino/src/domino1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class domino1 {
+
+	public static void main(String[] args) {
+           int resultado;
+           Scanner entrada = new Scanner ( System.in);
+           int ancho=entrada.nextInt();
+           int largo=entrada.nextInt();
+           resultado=(ancho*largo)/2;
+           System.out.println(resultado);
+           
+           
+           
+	}
+
+}
